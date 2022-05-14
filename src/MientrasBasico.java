6764d07956e0f26174492377b41b896aeaebe5e2
Zuriel Villaneda <zvillanedamelendez@gmail.com>
2022-05-14 11:52:13 -0500

    Ciclo While

---
+++ src/MientrasBasico.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class MientrasBasico {
+    public static void main(String[] args){
+        int contandor = 0;
+        
+        while(contandor < 3){
+            System.out.println("contandor = " + contandor);
+            contandor++;
+        }
+    }
+}

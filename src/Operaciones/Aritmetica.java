a0ec7d28fbd7b62ff8168c8002db3c5fcabe0974
Zuriel Villaneda <zvillanedamelendez@gmail.com>
2022-05-18 22:18:07 -0500

    Introduccion a la creacion d epaquetes, metodos y atributos

---
+++ src/Operaciones/Aritmetica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operaciones;
+
+/**
+ *
+ * @author user
+ */
+public class Aritmetica {
+    int a, b;
+    
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+}

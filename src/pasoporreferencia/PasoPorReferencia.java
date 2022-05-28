97419ffda8251f1bbc647ad2ec1bd7ae44287e7b
Zuriel Villaneda <zvillanedamelendez@gmail.com>
2022-05-27 23:58:04 -0500

    Paso por referencia

---
+++ src/pasoporreferencia/PasoPorReferencia.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasoporreferencia;
+
+/**
+ *
+ * @author user
+ */
+public class PasoPorReferencia {
+    public static void main(String[] args) {
+        Persona perosna1 = new Persona ();
+    }
+}

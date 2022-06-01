1e293b4cd899ab3c3b20c5ba1d41987b02d952c1
Zuriel Villaneda <zvillanedamelendez@gmail.com>
2022-05-31 23:36:55 -0500

    Aprendiendo uso de this parte dos

--- src/palabrathis/PalabraThis.java
+++ src/palabrathis/PalabraThis.java
@@ -2,18 +2,30 @@
 package palabrathis;
 
 public class PalabraThis {
+
     public static void main(String[] args) {
         Persona persona = new Persona("Zuriel", "Villaneda");
-        System.out.print(persona);
+        System.out.println(persona);
         System.out.println("persona = " + persona.nombre);
         System.out.println("persona = " + persona.apellido);
-    }
-    
-}
-class Persona{
-    String nombre, apellido;
-    Persona(String nombre, String apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        new Imprimir().imprimir(persona);
     }
 }
+    class Persona {
+
+        String nombre, apellido;
+
+        Persona(String nombre, String apellido) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            System.out.println("this = " + this);
+        }
+    }
+
+    class Imprimir {
+
+        public void imprimir(Persona persona) {
+            System.out.println("persona = " + persona);
+            System.out.println("this = " + this);
+        }
+    }
